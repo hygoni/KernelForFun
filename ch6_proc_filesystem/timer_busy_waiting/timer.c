88464b03c79853f4f1e11cc8d52e9e527b55dd99
hygoni <9hrack@naver.com>
2021-04-11 21:54:52 +0900

    [ADD] add timer using jiffies + time_before (busy waiting)

---
+++ ch6_proc_filesystem/timer_busy_waiting/timer.c
@@ -0,0 +1,29 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/time.h>
+
+#define MODULE_NAME "TIMER"
+
+int __init timer_init(void) {
+	struct timespec64 ts;
+	unsigned long delay;
+
+	ktime_get_ts64(&ts);
+	printk(KERN_INFO "[%s] %s currnt time: %llu\n", MODULE_NAME, __func__, ts.tv_sec);
+	printk(KERN_INFO "[%s] %s waiting 3 seconds...\n", MODULE_NAME, __func__);
+
+	delay = jiffies + 3 * HZ;
+	while (time_before(jiffies, delay));
+	ktime_get_ts64(&ts);
+	printk(KERN_INFO "[%s] %s currnt time: %llu\n", MODULE_NAME, __func__, ts.tv_sec);
+	return 0;
+}
+
+void __exit timer_exit(void) {
+	
+}
+
+module_init(timer_init);
+module_exit(timer_exit);
+
+MODULE_LICENSE("GPL");
